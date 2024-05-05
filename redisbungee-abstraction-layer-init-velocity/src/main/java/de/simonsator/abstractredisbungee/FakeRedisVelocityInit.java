8df94bb9133ed448c0ca3aff025ceb779d60c420
Simonsator <00pflaume@gmail.com>
2024-05-05 16:51:25 +0200

    Load velocity unífied

--- redisbungee-abstraction-layer-init-velocity/src/main/java/de/simonsator/abstractredisbungee/FakeRedisVelocityInit.java
+++ redisbungee-abstraction-layer-init-velocity/src/main/java/de/simonsator/abstractredisbungee/FakeRedisVelocityInit.java
@@ -3,11 +3,16 @@ package de.simonsator.abstractredisbungee;
 import com.velocitypowered.api.proxy.ProxyServer;
 import de.simonsator.abstractredisbungee.invk.InvvkRedisVelocityAPI;
 import de.simonsator.abstractredisbungee.velocity.limework.LimeworkRedisVelocityAPI;
+import de.simonsator.abstractredisbungee.velocity.limework.LimeworkRedisVelocityAPIUnifiedJedis;
 
 public class FakeRedisVelocityInit {
 	public static boolean init(ProxyServer pProxyServer, Object pMainPluginObject) {
 		if (InvvkRedisVelocityAPI.isCompatible()) {
 			new InvvkRedisVelocityAPI(pProxyServer, pMainPluginObject);
+			return true;
+		}
+		if (LimeworkRedisVelocityAPIUnifiedJedis.isCompatible()) {
+			new LimeworkRedisVelocityAPIUnifiedJedis(pProxyServer, pMainPluginObject);
 			return true;
 		}
 		if (LimeworkRedisVelocityAPI.isCompatible()) {
