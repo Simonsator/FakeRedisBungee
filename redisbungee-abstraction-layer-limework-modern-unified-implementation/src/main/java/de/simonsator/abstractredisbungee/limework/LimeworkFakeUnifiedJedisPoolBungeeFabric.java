49110ef80e91a2cdb012ab86a28115e847d2a62b
Simonsator <00pflaume@gmail.com>
2024-06-06 01:59:37 +0200

    Use sumoner

--- redisbungee-abstraction-layer-limework-modern-unified-implementation/src/main/java/de/simonsator/abstractredisbungee/limework/LimeworkFakeUnifiedJedisPoolBungeeFabric.java
+++ redisbungee-abstraction-layer-limework-modern-unified-implementation/src/main/java/de/simonsator/abstractredisbungee/limework/LimeworkFakeUnifiedJedisPoolBungeeFabric.java
@@ -1,18 +1,11 @@
 package de.simonsator.abstractredisbungee.limework;
 
-import com.imaginarycode.minecraft.redisbungee.RedisBungee;
+import com.imaginarycode.minecraft.redisbungee.RedisBungeeAPI;
 import de.simonsator.abstractredisbungee.limework.common.unified.LimeworkFakeUnifiedJedisPool;
-import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.plugin.Plugin;
 
 public class LimeworkFakeUnifiedJedisPoolBungeeFabric {
 	public static LimeworkFakeUnifiedJedisPool create() {
-		Plugin redisBungee = ProxyServer.getInstance().getPluginManager().getPlugin("RedisBungee");
-		if (redisBungee != null) {
-			RedisBungee redisBungeeInstance = (RedisBungee) redisBungee;
-			return new LimeworkFakeUnifiedJedisPool(redisBungeeInstance.proxyDataManager().unifiedJedis());
-		}
-		throw new RuntimeException("Could not load UnifiedJedisPool from Limework fork of RedisBungee");
+		return new LimeworkFakeUnifiedJedisPool(RedisBungeeAPI.getRedisBungeeApi().getSummoner().obtainResource());
 	}
 
 }
--- redisbungee-abstraction-layer-limework-redisvelocity-unified-implementation/src/main/java/de/simonsator/abstractredisbungee/velocity/limework/LimeworkFakeUnifiedJedisPoolVelocityFabric.java
+++ redisbungee-abstraction-layer-limework-redisvelocity-unified-implementation/src/main/java/de/simonsator/abstractredisbungee/velocity/limework/LimeworkFakeUnifiedJedisPoolVelocityFabric.java
@@ -1,17 +1,10 @@
 package de.simonsator.abstractredisbungee.velocity.limework;
 
-import com.imaginarycode.minecraft.redisbungee.RedisBungeeVelocityPlugin;
-import com.velocitypowered.api.plugin.PluginContainer;
-import com.velocitypowered.api.proxy.ProxyServer;
+import com.imaginarycode.minecraft.redisbungee.RedisBungeeAPI;
 import de.simonsator.abstractredisbungee.limework.common.unified.LimeworkFakeUnifiedJedisPool;
 
 public class LimeworkFakeUnifiedJedisPoolVelocityFabric {
-	public static LimeworkFakeUnifiedJedisPool create(ProxyServer pProxyServer) {
-		return pProxyServer.getPluginManager().getPlugin("redisbungee")
-				.flatMap(PluginContainer::getInstance)
-				.filter(instance -> instance instanceof RedisBungeeVelocityPlugin)
-				.map(instance -> (RedisBungeeVelocityPlugin) instance)
-				.map(pluginInstance -> new LimeworkFakeUnifiedJedisPool(pluginInstance.proxyDataManager().unifiedJedis()))
-				.orElseThrow(() -> new RuntimeException("Could not load UnifiedJedisPool from Limework fork of RedisBungee"));
+	public static LimeworkFakeUnifiedJedisPool create() {
+		return new LimeworkFakeUnifiedJedisPool(RedisBungeeAPI.getRedisBungeeApi().getSummoner().obtainResource());
 	}
 }
